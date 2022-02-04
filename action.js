a6033e6b09ffd1dadc92d5906adeb10dcad34e74
safarh02122002 <99020075+safarh02122002@users.noreply.github.com>
2022-02-04 17:04:11 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,27 @@
+function ChangeToOrange()
+{
+    document.getElementById("btn1").style.backgroundColor="orange";
+    
+}
+function ChangeToBlack()
+{
+    document.getElementById("btn1").style.backgroundColor="black";
+}
+function ChangeToOrange2()
+{
+    
+    document.getElementById("btn2").style.backgroundColor="orange";
+}
+function ChangeToBlack2()
+{
+    document.getElementById("btn2").style.backgroundColor="black";
+}
+function ChangeToOrange3()
+{
+    document.getElementById("btn3").style.backgroundColor="orange";
+    
+}
+function ChangeToBlack3()
+{
+    document.getElementById("btn3").style.backgroundColor="black";
+}
